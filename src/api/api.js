00ee1062c3dc5af361118b9b9d098cd779f27854
Mairacol <120331883+Mairacol@users.noreply.github.com>
2024-12-22 11:25:12 -0300

    baseURL modificado

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Cambia esta URL cuando el back-end esté desplegado en producción
+  baseURL: 'http://localhost:3000/api', // Cambia esta URL cuando el back-end esté desplegado en producción
 });
 
 export default api;
