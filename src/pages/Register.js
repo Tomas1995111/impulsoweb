b33573caf228730d84c07e2654489faba876a1b4
Mairacol <120331883+Mairacol@users.noreply.github.com>
2025-03-12 20:09:17 -0300

    register estilizado

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,15 +1,14 @@
-// Register.js
-
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './styles/Register.css'; // Importamos el archivo de estilos para el registro
 
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const navigate = useNavigate();  // Usar useNavigate para la redirección
+  const navigate = useNavigate(); // Usar useNavigate para la redirección
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,29 +29,44 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>Registro</h2>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Nombre"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Registrarse</button>
+    <div className="register-container">
+      <h2 className="register-title">Registro</h2>
+      {error && <p className="error-message">{error}</p>}
+      <form onSubmit={handleSubmit} className="register-form">
+        <div className="input-group">
+          <label>Nombre:</label>
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="input-field"
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label>Correo electrónico:</label>
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input-field"
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input-field"
+            required
+          />
+        </div>
+        <button type="submit" className="submit-btn">Registrarse</button>
       </form>
     </div>
   );
